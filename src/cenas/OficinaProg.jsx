9c5f3a1e66a6a3176d02a839e8c7344094696092
tai <anatainarabat@gmail.com>
2026-05-03 15:20:56 -0300

    feat: Quadrinhos da oficina de programação implementados

--- src/cenas/OficinaProg.jsx
+++ src/cenas/OficinaProg.jsx
@@ -1,9 +1,52 @@
+import { useEffect } from "react";
 import { motion } from "framer-motion";
 import Quadro from "../Quadro";
 import fundo from '../assets/intro/HALL.png'
 import '../styles/ofc-programacao.css';
 
+// Cena 1
+import CENA1FUNDO from '../assets/programacao/CENA1_FUNDO.png';
+import CENA1FALA1 from '../assets/programacao/CENA1_FALA1.png';
+import CENA1FALA2 from '../assets/programacao/CENA1_FALA2.png';
+
+
+// Cena 2
+import CENA2FUNDO from '../assets/programacao/CENA2_FUNDO.png';
+import CENA2FALA1 from '../assets/programacao/CENA2_FALA1.png';
+import CENA2FALA2 from '../assets/programacao/CENA2_FALA2.png';
+
+
+// Cena 3
+import CENA3FALA from '../assets/programacao/CENA3_FALA.png';
+
+
+// Cena 4 
+import CENA4FUNDO from '../assets/programacao/CENA4_fundo.png';
+import CENA4FALA1 from '../assets/programacao/CENA4_FALA1.png';
+import CENA4FALA2 from '../assets/programacao/CENA4_FALA2.png';
+
+// Cena 5
+import CENA5FUNDO from '../assets/programacao/CENA5_FUNDO.png';
+import CENA5FALA1 from '../assets/programacao/CENA5_FALA1.png';
+import CENA5FALA2 from '../assets/programacao/CENA5_FALA2.png';
+
+// Cena 6
+import CENA6FUNDO from '../assets/programacao/CENA6_FUNDO.png';
+import CENA6FALA from '../assets/programacao/CENA6_FALA.png';
+
+
 export default function OficinaProgramacao({ cena, setCena, setComputadorAberto, cenaAnim }) {
+
+  useEffect(() => {
+    if (cena === "oficina_programacao_final") {
+      const timer = setTimeout(() => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      }, 150);
+
+      return () => clearTimeout(timer);
+    }
+  }, [cena]);
+
   return (
     <motion.div key="programacao" {...cenaAnim} className="story-container">
 
@@ -11,20 +54,60 @@ export default function OficinaProgramacao({ cena, setCena, setComputadorAberto,
       {cena === "oficina_programacao" && (
         <>
 
-          <Quadro bgImage={fundo}>
+          <Quadro
+            bgImage={CENA1FUNDO}
+            baloes={[CENA1FALA1, CENA1FALA2]}
+            balaoStyle={[
+              {
+                marginRight: "2%",
+                marginBottom: "15%",
+                maxWidth: "250px",
+                height: "250px"
+              },
+              {
+                marginLeft: "28%",
+                marginBottom: "5%",
+                maxWidth: "200px",
+                height: "250px"
+              }
+            ]}
+          />
+
+          <Quadro
+            bgImage={CENA2FUNDO}
+            baloes={[CENA2FALA1, CENA2FALA2]}
+            balaoStyle={[
+              {
+                marginLeft: "5%",
+                marginBottom: "25%",
+                maxWidth: "250px",
+                height: "270px"
+              },
+              {
+                marginLeft: "30%",
+                marginRight: "5%",
+                marginBottom: "5%",
+                maxWidth: "200px",
+                height: "250px"
+              }
+            ]}
+          />
+
+          {/* BOTÕES */}
+          <Quadro bgImage={CENA4FUNDO}>
             <div className="botoes-container">
-              <button 
+              <button
                 className="button-geral"
                 onClick={() => setComputadorAberto(true)}
               >
-                Usar computador
+                Que legal, Vamos codar!
               </button>
 
-              <button 
+              <button
                 className="button-geral btn-back"
                 onClick={() => setCena("corredor")}
               >
-                Voltar
+                Humm... acho que vou olhar outras oficinas.
               </button>
             </div>
           </Quadro>
@@ -34,32 +117,50 @@ export default function OficinaProgramacao({ cena, setCena, setComputadorAberto,
       {/* FINAL DA OFICINA */}
       {cena === "oficina_programacao_final" && (
         <>
-          <Quadro bgImage={fundo}>
-            <h2>Eve: "Caramba, muito bem! Nem precisou da minha ajuda."</h2>
-          </Quadro>
+          <Quadro
+            bgImage={CENA5FUNDO}
+            baloes={[CENA5FALA1, CENA5FALA2]}
+            balaoStyle={[
+              {
+                marginLeft: "1%",
+                marginBottom: "25%",
+                maxWidth: "250px",
+                height: "150px"
+              },
+              {
+                marginLeft: "30%",
+                marginRight: "5%",
+                marginBottom: "35%",
+                maxWidth: "200px",
+                height: "150px"
+              }
+            ]}
+          />
 
-          <Quadro bgImage={fundo}>
-            <h2>"Se você não sabe o que seguir… programação é uma ótima escolha."</h2>
-          </Quadro>
-
-          <Quadro bgImage={fundo}>
-            <h2>"E… bom… a gente ia se ver bastante nas aulas..."</h2>
-          </Quadro>
-
-          <Quadro bgImage={fundo}>
+          <Quadro
+            bgImage={CENA6FUNDO}
+            baloes={[CENA6FALA]}
+            balaoStyle={[
+              {
+                marginRight: "50%",
+                marginBottom: "15%",
+                maxWidth: "250px",
+                height: "200px"
+              }]}
+          >
             <div className="decisao-botoes">
-              <button 
+              <button
                 className="button-geral"
                 onClick={() => setCena("final_ru")}
               >
                 Bora pro RU!
               </button>
 
-              <button 
+              <button
                 className="button-geral"
                 onClick={() => setCena("corredor")}
               >
-                 Obrigada pelo convite, mas ainda quero ver mais labs.
+                Obrigada pelo convite, mas vou ver outras oficinas.
               </button>
             </div>
           </Quadro>
